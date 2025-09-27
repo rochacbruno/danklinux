5b1f3f9006a63690cd71d8354cee92f83bb88d35
bbedward <bbedward@gmail.com>
2025-09-26 21:24:03 -0400

    gaps 4 by default

--- internal/config/niri.go
+++ internal/config/niri.go
@@ -47,7 +47,7 @@ input {
 // https://github.com/YaLTeR/niri/wiki/Configuration:-Layout
 layout {
     // Set gaps around windows in logical pixels.
-    gaps 5
+    gaps 4
     background-color "transparent"
     // When to center a column when changing focus, options are:
     // - "never", default behavior, focusing an off-screen column will keep at the left
